6c094afa707ff9d59e02e2baf8e2302dc240c829
Jingxi Xu <zealliant@outlook.com>
2019-02-18 00:53:06 -0500

    increase pose pulisher frequency to 100hz; achieve very smooth conveyor motion; can model speed 0.4m/s

--- move_conveyor.py
+++ move_conveyor.py
@@ -14,6 +14,8 @@ mico_moveit = mico_moveit.MicoMoveit()
 ut.remove_all_constraints()
 
 MOVE = True # if not MOVE, just update scene
+BACK_AND_FORCE = False # if false, quit after exp finishes
+PRINT_SPEED = True # print speed only once
 
 if __name__ == "__main__":
 
@@ -23,15 +25,17 @@ if __name__ == "__main__":
     mico_moveit.add_box("floor", ((0, 0, -0.005), (0, 0, 0, 1)), size=(2, 2, 0.01))
     pub = rospy.Publisher('target_pose', Pose, queue_size=1)
 
-    cube = ut.get_body_id("cube_small_modified")
+    target = ut.get_body_id("cube_small_modified")
     conveyor = ut.get_body_id("conveyor")
 
-    speed = 0.02 # m/s
+    speed = 0.03 # m/s
+    rate = 100
+    r = rospy.Rate(rate)  # publishing frequency
 
     # distance along x to travel
     max_x = 0.8
     min_x = -0.8
-    step = speed/10.0 # meters per 0.1 seconds
+    step = speed/float(rate) # meters per step
 
     pivot = ut.get_body_pose(conveyor)[0]
 
@@ -41,7 +45,6 @@ if __name__ == "__main__":
                                  parentFramePosition=[0, 0, 0], childFramePosition=pivot)
         direction = '+' # for moving back and force
         c = time.time()
-        print_speed = True  # print speed only once
         while True:
             # print(direction)
             # print(pivot[0])
@@ -51,29 +54,36 @@ if __name__ == "__main__":
                 pivot[0] = pivot[0] - step
             # p.changeConstraint(cid, pivot, jointChildFrameOrientation=orn, maxForce=50)
             p.changeConstraint(cid, pivot, maxForce=5000)
-            if pivot[0] > max_x and print_speed:
-                direction = "-"
-                time_spent = time.time() - c
-                speed = (max_x - min_x) / time_spent
-                rospy.loginfo("real speed: {} m/s".format(speed))
-                flag = False
+
+            target_pose_2d = ut.get_body_pose(target)
+            conveyor_pose_2d = ut.get_body_pose(conveyor)
+            target_pose = ut.list_2_pose(target_pose_2d)
+            pub.publish(target_pose)
+
+            mico_moveit.add_box("cube", target_pose_2d, size=(0.05, 0.05, 0.05))
+            mico_moveit.add_box("conveyor", conveyor_pose_2d, size=(.1, .1, .02))
+
+            if pivot[0] > max_x:
+                if PRINT_SPEED:
+                    time_spent = time.time() - c
+                    speed = (conveyor_pose_2d[0][0] - min_x) / time_spent
+                    rospy.loginfo("real speed: {} m/s".format(speed))
+                    PRINT_SPEED = False  # only print once
+                if BACK_AND_FORCE:
+                    direction = "-"
+                else:
+                    exit(0)
             elif pivot[0] < min_x:
                 direction = "+"
-
-            target_pose_2d = ut.get_body_pose(ut.get_body_id('cube_small_modified'))
-            target_pose = ut.list_2_pose(target_pose_2d)
-            pub.publish(target_pose)
-
-            mico_moveit.add_box("cube", p.getBasePositionAndOrientation(cube), size=(0.05, 0.05, 0.05))
-            mico_moveit.add_box("conveyor", p.getBasePositionAndOrientation(conveyor), size=(.1, .1, .02))
-            time.sleep(.1)
+            r.sleep()
     else:
         while True:
-            target_pose_2d = ut.get_body_pose(ut.get_body_id('cube_small_modified'))
+            target_pose_2d = ut.get_body_pose(target)
+            conveyor_pose_2d = ut.get_body_pose(conveyor)
             target_pose = ut.list_2_pose(target_pose_2d)
             pub.publish(target_pose)
-            mico_moveit.add_box("cube", p.getBasePositionAndOrientation(cube), size=(0.05, 0.05, 0.05))
-            mico_moveit.add_box("conveyor", p.getBasePositionAndOrientation(conveyor), size=(.1, .1, .02))
+            mico_moveit.add_box("cube", target, size=(0.05, 0.05, 0.05))
+            mico_moveit.add_box("conveyor", conveyor, size=(.1, .1, .02))
             time.sleep(.1)
 
 
