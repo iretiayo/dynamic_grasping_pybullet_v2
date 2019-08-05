e5d776cc08bd32b403ad727fbeaa06787e73842f
Jingxi Xu <zealliant@outlook.com>
2019-08-04 22:41:09 -0400

    add comments to run_multiple

--- run_multiple.sh
+++ run_multiple.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
-iter=1
+iter=10
 online_planning=true
-# all
+### all
 #for object_name in cube bleach_cleanser bowl cracker_box master_chef_can mug mustard_bottle potted_meat_can power_drill pudding_box sugar_box tomato_soup_can
-#bowl, cracker_box blocked, pudding box weird starting pose
-# worshop objects set
-#for object_name in bleach_cleanser mug mustard_bottle potted_meat_can sugar_box tomato_soup_can cube power_drill
-for object_name in sugar_box
+### bowl, cracker_box blocked, pudding box weird starting pose
+### worshop objects set
+for object_name in bleach_cleanser mug mustard_bottle potted_meat_can sugar_box tomato_soup_can cube power_drill
+#for object_name in bleach_cleanser
 do
-    for conveyor_distance in 0.4
+    for conveyor_distance in 0.3, 0.4, 0.5
     do
-        for conveyor_velocity in 0.01
+        for conveyor_velocity in 0.01, 0.03, 0.05
         do
 
             if [  "$online_planning" = true  ] # if it is not running
