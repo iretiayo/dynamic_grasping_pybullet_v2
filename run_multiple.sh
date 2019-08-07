ed868edcf51f4d2bb37b9a8764862346ac190ec2
Jingxi Xu <zealliant@outlook.com>
2019-08-06 21:09:45 -0400

    fix grammar

--- run_multiple.sh
+++ run_multiple.sh
@@ -9,9 +9,9 @@ online_planning=true
 for object_name in bleach_cleanser mug mustard_bottle potted_meat_can sugar_box tomato_soup_can cube power_drill
 #for object_name in bleach_cleanser
 do
-    for conveyor_distance in 0.3, 0.4, 0.5
+    for conveyor_distance in 0.3 0.4 0.5
     do
-        for conveyor_velocity in 0.01, 0.03, 0.05
+        for conveyor_velocity in 0.01 0.03 0.05
         do
 
             if [  "$online_planning" = true  ] # if it is not running
