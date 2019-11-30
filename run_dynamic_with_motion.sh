85566dbf436d6f8f0fd79b6f1c3b539a39d73fb2
Jingxi Xu <zealliant@outlook.com>
2019-11-29 22:59:06 -0500

    script for linear motion test

---
+++ run_dynamic_with_motion.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+for object_name in bleach_cleanser mustard_bottle potted_meat_can sugar_box tomato_soup_can cube power_drill
+do
+    python run_dynamic_with_motion.py \
+    --object_name $object_name \
+    --rendering \
+    --num_trials 100 \
+    --result_dir result \
+    --grasp_database_path assets/grasps/filtered_grasps
+    sleep 5
+done
