9861b097560a9d5d7f735cdeb0211224db6212d4
Jingxi Xu <zealliant@outlook.com>
2019-11-30 16:48:56 -0500

    add timestr

--- run_dynamic_with_motion.sh
+++ run_dynamic_with_motion.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
+timestr=$(date '+%Y-%m-%d_%H-%M-%S')
 for object_name in bleach_cleanser mustard_bottle potted_meat_can sugar_box tomato_soup_can cube power_drill
 do
     python run_dynamic_with_motion.py \
     --object_name $object_name \
     --rendering \
     --num_trials 100 \
-    --result_dir result \
+    --result_dir $timestr \
     --grasp_database_path assets/grasps/filtered_grasps \
     --grasp_threshold 0.03 \
     --lazy_threshold  0.3 \
