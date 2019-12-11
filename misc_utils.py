200a62f19e998a818125d286a62f8132ff397fd4
Jingxi Xu <zealliant@outlook.com>
2019-12-10 23:02:56 -0500

    fix bug

--- misc_utils.py
+++ misc_utils.py
@@ -7,3 +7,27 @@ import pybullet as p
 import pybullet_utils as pu
 
 
+def write_csv_line(result_file_path, result):
+    """ write a line in a csv file; create the file and write the first line if the file does not already exist """
+    pp = pprint.PrettyPrinter(indent=4)
+    pp.pprint(result)
+    result = OrderedDict(result)
+    file_exists = os.path.exists(result_file_path)
+    with open(result_file_path, 'a') as csv_file:
+        writer = csv.DictWriter(csv_file, result.keys())
+        if not file_exists:
+            writer.writeheader()
+        writer.writerow(result)
+
+
+def configure_pybullet(rendering=False):
+    if not rendering:
+        p.connect(p.DIRECT)
+    else:
+        p.connect(p.GUI_SERVER)
+    p.setAdditionalSearchPath(pybullet_data.getDataPath())
+    p.configureDebugVisualizer(p.COV_ENABLE_GUI, 0)
+    pu.reset_camera(yaw=50.0, pitch=-35.0, dist=1.200002670288086, target=(0.0, 0.0, 0.0))
+    p.setPhysicsEngineParameter(enableFileCaching=0)
+    p.resetSimulation()
+    p.setGravity(0, 0, -9.8)
