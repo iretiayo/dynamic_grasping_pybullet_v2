b63286be5f3e27bf2af749439fb00f64ca51a3af
Jingxi Xu <zealliant@outlook.com>
2019-12-13 14:55:02 -0500

    update

--- run_static_with_motion.sh
+++ run_static_with_motion.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
+timestr=$(date '+%Y-%m-%d_%H-%M-%S')
 for object_name in bleach_cleanser mustard_bottle potted_meat_can sugar_box tomato_soup_can cube power_drill
 do
     python run_static_with_motion.py \
     --object_name $object_name \
     --rendering \
     --num_trials 500 \
-    --result_dir result \
-    --grasp_database_path assets/grasps/filtered_grasps;
+    --result_dir $timestr \
+    --grasp_database_path assets/grasps/filtered_grasps_noise;
     sleep 5
 done
