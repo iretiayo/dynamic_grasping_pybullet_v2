5770b700fc846d3a7ae1f813d5caae81a5715c94
Jingxi Xu <zealliant@outlook.com>
2019-12-16 21:55:19 -0500

    update

--- collect_good_grasps.sh
+++ collect_good_grasps.sh
@@ -5,11 +5,14 @@ do
     python collect_good_grasps.py \
     --object_name $object_name \
     --load_folder_path assets/grasps/raw_grasps \
-    --save_folder_path assets/grasps/filtered_grasps_noise \
-    --min_success_rate 0.95 \
+    --save_folder_path assets/grasps/filtered_grasps_noise_100 \
+    --min_success_rate 1.0 \
     --num_trials 50 \
     --num_grasps 5000 \
+    --num_successful_grasps 100 \
     --apply_noise \
+    --prior_folder_path assets/grasps/filtered_grasps_noise_5000_1.00 \
+    --prior_success_rate 1.0 \
     --back_off 0.05;
     sleep 5;
 done
