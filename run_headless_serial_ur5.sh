16a5a1d80e97c47a9ddc6750b55eaf0cdfb6e34b
iretiayo <iretibob@gmail.com>
2020-10-28 14:24:59 -0400

    add headless launch script

---
+++ run_headless_serial_ur5.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+exp_name="exp_name"
+timestr=${exp_name}_ur5_$(hostname)_$(date '+%Y-%m-%d_%H-%M-%S')
+
+mkdir $timestr
+cp run_headless_serial.sh $timestr
+
+node_id=50000
+
+screen -d -m -S ${timestr}_moveit bash -c "source ../../devel/setup.bash && \
+    export ROS_MASTER_URI=http://localhost:${node_id} && \
+    roslaunch launch/ur5_robotic_moveit_ros.launch planner:=ompl;"
+
+sleep 3
+
+source ../../devel/setup.bash
+export ROS_MASTER_URI=http://localhost:$node_id
+
+for object_name in bleach_cleanser mustard_bottle potted_meat_can sugar_box tomato_soup_can cube power_drill; do
+  python run_dynamic_with_motion.py \
+    --object_name $object_name \
+    --robot_config_name ur5_robotiq \
+    --num_trials 100 \
+    --result_dir $timestr \
+    --grasp_database_path assets/grasps/filtered_grasps_noise_robotiq_100_1.00 \
+    --baseline_experiment_path ur5_dynamic_medium_speed_z_motion \
+    --grasp_threshold 0.1 \
+    --lazy_threshold 30.3 \
+    --conveyor_speed 0.03 \
+    --close_delay 0.5 \
+    --back_off -0.075 \
+    --distance_low 0.3 \
+    --distance_high 0.7 \
+    --pose_freq 5 \
+    --use_previous_jv \
+    --use_seed_trajectory \
+    --max_check 3 \
+    --use_box \
+    --use_kf \
+    --fix_motion_planning_time 0.14 \
+    --fix_grasp_ranking_time 0.135;
+  sleep 5
+done
