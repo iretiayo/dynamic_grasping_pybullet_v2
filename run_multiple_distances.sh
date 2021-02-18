792c7e0d1143ddd5991637c3b9c1037458f614bb
Jingxi Xu <zealliant@outlook.com>
2021-02-17 20:07:25 -0800

    update

--- run_multiple_distances.sh
+++ run_multiple_distances.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 node_id=10000
-# safe distance for ur5 is >= 0.25
-for distance_low in 0.25 0.30 0.35; do
+# safe distance for ur5 is 0.25 <= 0.90
+for distance_low in 0.25 0.30 0.35 0.40 0.45 0.50 0.55 0.60 0.65 0.70 0.75 0.80 0.85; do
   incremental=0.05;
   distance_high=$( bc <<<"$distance_low + $incremental" );
   ./run_single.sh \
