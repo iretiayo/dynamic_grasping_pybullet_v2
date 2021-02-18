59051a64c71acbf0e37390edd30fb4913cb5b36a
Jingxi Xu <zealliant@outlook.com>
2021-02-17 19:51:37 -0800

    update script

--- run_multiple_distances.sh
+++ run_multiple_distances.sh
@@ -9,7 +9,9 @@ for distance_low in 0.25 0.30 0.35 0.40 0.45 0.50 0.55 0.60 0.65 0.70 0.75 0.80 
     --node_id ${node_id} \
     --exp_name ${distance_low}_${distance_high} \
     --distance_low ${distance_low} \
-    --distance_high ${distance_high};
+    --distance_high ${distance_high} \
+    --record_video true \
+    --rendering true;
   echo $node_id;
   echo ${distance_low};
   echo ${distance_high};
