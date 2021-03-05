f4bc032ac59afe851919056449033e85ae48bf51
Jingxi Xu <zealliant@outlook.com>
2021-03-05 02:13:42 -0800

    update

--- run_multiple_distances_mico.sh
+++ run_multiple_distances_mico.sh
@@ -2,19 +2,16 @@
 
 node_id=10000
 # safe distance for ur5 is 0.25 <= 0.90
-for distance_low in 0.25 0.30 0.35 0.40 0.45 0.50 0.55 0.60 0.65 0.70 0.75; do
+for distance_low in 0.15 0.20 0.25 0.30 0.35 0.40 0.45 0.50; do
   incremental=0.05;
   distance_high=$( bc <<<"$distance_low + $incremental" );
-  ./run_single.sh \
-    --node_id ${node_id} \
+  ./run_single_mico.sh --node_id ${node_id} \
     --exp_name ur5_${distance_low}_${distance_high} \
     --motion_mode dynamic_linear \
-    --distance_low ${distance_low} \
-    --distance_high ${distance_high} \
     --use_reachability true \
     --use_motion_aware false \
-    --conveyor_speed 0.05 ;
-    --baseline_experiment_path assets/benchmark_tasks/ur5_robotiq/linear_obstacles;
+    --baseline_experiment_path assets/benchmark_tasks/mico/linear_tasks_mico \
+    --conveyor_speed 0.03;
   echo $node_id;
   echo ${distance_low};
   echo ${distance_high};
